68d0abfebbd11e99704e0c90e5a4da66b0f5b89c
msejuro312 <msejuro312@protonmail.com>
2026-06-18 22:00:41 -0500

    Primer commit, configuración, tipo y usuario

---
+++ src/test/java/com/serfagab/ProyectoLp2GrupoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serfagab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLp2GrupoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
